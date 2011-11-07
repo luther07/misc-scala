8d50a25af8f064d13193e5b21236150fdb4757e1
Mark Johnson <luther07@gmail.com>
2011-11-06 20:13:57 -0600

    included statement rule from imperative dir, which may need to be extended for expression

--- src/records/parser.scala
+++ src/records/parser.scala
@@ -3,5 +3,9 @@ package records
 import scala.util.parsing.combinator._
 
 object StatementParser extends JavaTokenParsers {
-
+  def statement: Parser[Statement] = (
+    ident ~ "=" ~ expr ^^ { case s ~ _ ~ r => Assignment(Variable(s), r) }
+  | "while" ~ "(" ~> expr ~ ")" ~ statement ^^ { case g ~ _ ~ b => While(g, b) }
+  | "{" ~> repsep(statement, ",") <~ "}" ^^ { case ss => Sequence(ss: _*) }
+  )
 }
