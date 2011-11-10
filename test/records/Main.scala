2b3ffb15311840f92bf50b6686442b197d134627
gvasels <vaselsg@gmail.com>
2011-11-10 17:14:50 -0600

    Was trying to migrate the test framework over :) I think the push and pull work now as it should?

---
+++ test/records/Main.scala
@@ -0,0 +1,8 @@
+package edu.luc.cs.laufer.cs473.shapealgebra
+
+
+object Main {
+  def main(args : Array[String]) : Unit = {
+    
+  }
+}
---
+++ test/records/TestRecords.scala
@@ -0,0 +1,13 @@
+package edu.luc.cs.laufer.cs473.shapealgebra
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+import java.awt.image.BufferedImage
+
+
+@RunWith(classOf[JUnitRunner])
+class TestRecords extends FunSuite{
+  
+}
