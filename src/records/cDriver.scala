2100cb3715759c32c98774d8bcd771e6dcb1d9aa
Mark Johnson <luther07@gmail.com>
2011-11-12 21:36:01 -0600

    added comment, purpose of this file is only for debugging

--- src/records/cDriver.scala
+++ src/records/cDriver.scala
@@ -2,6 +2,11 @@ package records
 
 import java.io.FileReader
 
+/* When executed with an argument that is the name of a file,
+   this main method will print the result of parsing the file.
+   Printing the result of parsing a file is useful only for 
+   debugging. */ 
+
 object CParse extends StatementParser {
   def main(args: Array[String]) {
     val reader = new FileReader(args(0))
