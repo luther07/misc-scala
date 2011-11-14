6738df8c43ead2ca60213296b147b3533f6c15f7
gvasels <vaselsg@gmail.com>
2011-11-14 00:41:36 -0600

    repl readline err

--- src/records/replIO.scala
+++ src/records/replIO.scala
@@ -1,24 +1,30 @@
 package records
 
 object replIO {
-  def myRepl() {
+  def myRepl() : String = {
+    var x : String = "";
     while(true){
       println("Please Enter a command. Type 'help;' for more information")
-      val x = readLine() match {
+      x  = readLine() match {
+        
         case repl.help => {
           println("Interpreter help. You must type a command for an action to occur, followed by a semicolon (;)");
           println("help; brings up this screen. quit; terminates the program.");
-          println("clear; will reset the store and dump; will display the contents of the store")
+          println("clear; will reset the store and dump; will display the contents of the store");
         }
         case repl.quit => System.exit(0)
         case repl.clear => "";
         case repl.dump => "";
         case _ => {
-          println("You selected an incorrect key... Please select again.");
-          myRepl();
+          interpret(x)
         }
       }
     }
+    return x;
+  }
+  
+  def interpret(line : String) {
+    
   }
   
   def main(args: Array[String]) {
@@ -26,9 +32,14 @@ object replIO {
   }
   
   object repl {
-  val help = "help;";
+  val help  = "help;";
   val quit = "quit;";
   val clear = "clear;";
   val dump = "dump;";
 }
+ 
+  val store = Map[String, Cell](
+    
+  )
+  
 }
