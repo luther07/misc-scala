728d67b60287fab95bc6be2ffa79d8c435f7e470
gvasels <vaselsg@gmail.com>
2011-11-13 22:15:02 -0600

    Update on REPL

--- src/records/repl.scala
+++ src/records/repl.scala
@@ -7,17 +7,28 @@ object repl {
   val dump = "dump;";
 }
  
-class replRunner() {
+object replRunner {
   def myRepl() {
     while(true){
       println("Please Enter a command. Type 'help;' for more information")
       val x = readLine() match {
-        case repl.help => "";
-        case repl.quit => "";
+        case repl.help => {
+          println("Interpreter help. You must type a command for an action to occur, followed by a semicolon (;)");
+          println("help; brings up this screen. quit; terminates the program.");
+          println("clear; will reset the store and dump; will display the contents of the store")
+        }
+        case repl.quit => System.exit(0)
         case repl.clear => "";
         case repl.dump => "";
-        case _ => "";
+        case _ => {
+          println("You selected an incorrect key... Please select again.");
+          myRepl();
+        }
       }
     }
+  }
+  
+  def main(args : Array[String]) : Unit = {
+    myRepl()
   }
 }
